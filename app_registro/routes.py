bdc774e0a12fec1b4907993bdba0eb6f68c07c8c
albertferal <albertferal@gmail.com>
2022-12-22 04:29:50 +0100

    quedan 41 min para terminar la clase

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -4,8 +4,28 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #prueba de diccionario a vista html
+    datos = [
+        {
+            "Fecha": "18/12/2022",
+            "Concepto": "Regalo de reyes",
+            "Cantidad": "-275,50"
+        }, 
+        {
+            "Fecha": "19/12/2022",
+            "Concepto": "Cobro de trabajo",
+            "Cantidad": "1200"
+        }, 
+        {
+            "Fecha": "18/12/2022",
+            "Concepto": "Ropa navidad",
+            "Cantidad": "-355,50"
+        }
+    ]
+    return render_template("index.html", pageTitle = "Listas", lista = datos)
+
+
 
 @app.route("/new")
 def create():
-    return render_template("new.html")
+    return render_template("new.html", pageTitle = "Alta")
